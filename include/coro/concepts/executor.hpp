4d00261dcfc3759e72e4bceb20df659fda8394a0
jbaldwin <joshrbaldwin@gmail.com>
2024-01-11 14:49:10 -0700

    Introduce concepts::io_scheduler
    The dns resolver hard links against coro::io_scheduler via coro::task_container<coro::io_scheduler>. Since the concepts::executor was introduced a while ago to make it possible to pass in different executors (or even user defined executors) the resolver has apparently been forgotten about. Lets introduce an io_executor (needs poll()) and make the resolver use an agnostic concepts::executor.
    
    The other reason for doing this is that the windows build is failing when LIBCORO_FEATURE_PLATFORM=OFF and building as a SHARED library since the coro::io_scheduler is sneaking in via the resolver class but then ultimately isn't compiled since its excluded from the build.
    
    Closes #237

--- include/coro/concepts/executor.hpp
+++ include/coro/concepts/executor.hpp
@@ -1,24 +1,31 @@
 #pragma once
 
 #include "coro/concepts/awaitable.hpp"
+#include "coro/fd.hpp"
+#include "coro/poll.hpp"
+#include "coro/task.hpp"
 
+#include <chrono>
 #include <concepts>
 #include <coroutine>
 
 namespace coro::concepts
 {
+
+// clang-format off
 template<typename type>
 concept executor = requires(type t, std::coroutine_handle<> c)
 {
-    {
-        t.schedule()
-        } -> coro::concepts::awaiter;
-    {
-        t.yield()
-        } -> coro::concepts::awaiter;
-    {
-        t.resume(c)
-        } -> std::same_as<void>;
+    { t.schedule() } -> coro::concepts::awaiter;
+    { t.yield() } -> coro::concepts::awaiter;
+    { t.resume(c) } -> std::same_as<void>;
 };
+
+template<typename type>
+concept io_exceutor = executor<type> and requires(type t, std::coroutine_handle<> c, fd_t fd, coro::poll_op op, std::chrono::milliseconds timeout)
+{
+    { t.poll(fd, op, timeout) } -> std::same_as<coro::task<poll_status>>;
+};
+// clang-format on
 
 } // namespace coro::concepts
--- include/coro/net/dns/resolver.hpp
+++ include/coro/net/dns/resolver.hpp
@@ -1,13 +1,17 @@
 #pragma once
 
+#include "coro/concepts/executor.hpp"
+#include "coro/event.hpp"
 #include "coro/fd.hpp"
-#include "coro/io_scheduler.hpp"
 #include "coro/net/hostname.hpp"
 #include "coro/net/ip_address.hpp"
+#include "coro/poll.hpp"
 #include "coro/task.hpp"
 #include "coro/task_container.hpp"
 
 #include <ares.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 #include <array>
 #include <chrono>
@@ -20,6 +24,15 @@
 
 namespace coro::net::dns
 {
+namespace detail
+{
+/// Global count to track if c-ares has been initialized or cleaned up.
+static uint64_t m_ares_count;
+/// Critical section around the c-ares global init/cleanup to prevent heap corruption.
+static std::mutex m_ares_mutex;
+} // namespace detail
+
+template<concepts::io_exceutor executor_type>
 class resolver;
 
 enum class status
@@ -28,12 +41,18 @@ enum class status
     error
 };
 
+template<concepts::io_exceutor executor_type>
 class result
 {
-    friend resolver;
+    friend resolver<executor_type>;
 
 public:
-    result(coro::io_scheduler& scheduler, coro::event& resume, uint64_t pending_dns_requests);
+    result(executor_type& executor, coro::event& resume, uint64_t pending_dns_requests)
+        : m_executor(executor),
+          m_resume(resume),
+          m_pending_dns_requests(pending_dns_requests)
+    {
+    }
     ~result() = default;
 
     /**
@@ -48,7 +67,7 @@ public:
     auto ip_addresses() const -> const std::vector<coro::net::ip_address>& { return m_ip_addresses; }
 
 private:
-    coro::io_scheduler&                m_io_scheduler;
+    executor_type&                     m_executor;
     coro::event&                       m_resume;
     uint64_t                           m_pending_dns_requests{0};
     dns::status                        m_status{dns::status::complete};
@@ -57,24 +76,85 @@ private:
     friend auto ares_dns_callback(void* arg, int status, int timeouts, struct hostent* host) -> void;
 };
 
+template<concepts::io_exceutor executor_type>
 class resolver
 {
 public:
-    explicit resolver(std::shared_ptr<io_scheduler> scheduler, std::chrono::milliseconds timeout);
+    explicit resolver(std::shared_ptr<executor_type> executor, std::chrono::milliseconds timeout)
+        : m_executor(std::move(executor)),
+          m_timeout(timeout),
+          m_task_container(m_executor)
+    {
+        if (m_executor == nullptr)
+        {
+            throw std::runtime_error{"dns resolver cannot have nullptr executor"};
+        }
+
+        {
+            std::scoped_lock g{detail::m_ares_mutex};
+            if (detail::m_ares_count == 0)
+            {
+                auto ares_status = ares_library_init(ARES_LIB_INIT_ALL);
+                if (ares_status != ARES_SUCCESS)
+                {
+                    throw std::runtime_error{ares_strerror(ares_status)};
+                }
+            }
+            ++detail::m_ares_count;
+        }
+
+        auto channel_init_status = ares_init(&m_ares_channel);
+        if (channel_init_status != ARES_SUCCESS)
+        {
+            throw std::runtime_error{ares_strerror(channel_init_status)};
+        }
+    }
+
     resolver(const resolver&)                             = delete;
     resolver(resolver&&)                                  = delete;
     auto operator=(const resolver&) noexcept -> resolver& = delete;
     auto operator=(resolver&&) noexcept -> resolver&      = delete;
-    ~resolver();
+
+    ~resolver()
+    {
+        if (m_ares_channel != nullptr)
+        {
+            ares_destroy(m_ares_channel);
+            m_ares_channel = nullptr;
+        }
+
+        {
+            std::scoped_lock g{detail::m_ares_mutex};
+            --detail::m_ares_count;
+            if (detail::m_ares_count == 0)
+            {
+                ares_library_cleanup();
+            }
+        }
+    }
 
     /**
      * @param hn The hostname to resolve its ip addresses.
      */
-    auto host_by_name(const net::hostname& hn) -> coro::task<std::unique_ptr<result>>;
+    auto host_by_name(const net::hostname& hn) -> coro::task<std::unique_ptr<result<executor_type>>>
+    {
+        coro::event resume_event{};
+        auto        result_ptr = std::make_unique<result<executor_type>>(*m_executor.get(), resume_event, 2);
+
+        ares_gethostbyname(m_ares_channel, hn.data().data(), AF_INET, ares_dns_callback, result_ptr.get());
+        ares_gethostbyname(m_ares_channel, hn.data().data(), AF_INET6, ares_dns_callback, result_ptr.get());
+
+        // Add all required poll calls for ares to kick off the dns requests.
+        ares_poll();
+
+        // Suspend until this specific result is completed by ares.
+        co_await resume_event;
+        co_return result_ptr;
+    }
 
 private:
-    /// The io scheduler to drive the events for dns lookups.
-    std::shared_ptr<io_scheduler> m_io_scheduler;
+    /// The executor to drive the events for dns lookups.
+    std::shared_ptr<executor_type> m_executor;
 
     /// The global timeout per dns lookup request.
     std::chrono::milliseconds m_timeout{0};
@@ -86,15 +166,117 @@ private:
     /// are not setup when ares_poll() is called.
     std::unordered_set<fd_t> m_active_sockets{};
 
-    task_container<io_scheduler> m_task_container;
+    task_container<executor_type> m_task_container;
 
-    /// Global count to track if c-ares has been initialized or cleaned up.
-    static uint64_t m_ares_count;
-    /// Critical section around the c-ares global init/cleanup to prevent heap corruption.
-    static std::mutex m_ares_mutex;
+    auto ares_poll() -> void
+    {
+        std::array<ares_socket_t, ARES_GETSOCK_MAXNUM> ares_sockets{};
+        std::array<poll_op, ARES_GETSOCK_MAXNUM>       poll_ops{};
 
-    auto ares_poll() -> void;
-    auto make_poll_task(fd_t fd, poll_op ops) -> coro::task<void>;
+        int bitmask = ares_getsock(m_ares_channel, ares_sockets.data(), ARES_GETSOCK_MAXNUM);
+
+        size_t new_sockets{0};
+
+        for (size_t i = 0; i < ARES_GETSOCK_MAXNUM; ++i)
+        {
+            uint64_t ops{0};
+
+            if (ARES_GETSOCK_READABLE(bitmask, i))
+            {
+                ops |= static_cast<uint64_t>(poll_op::read);
+            }
+            if (ARES_GETSOCK_WRITABLE(bitmask, i))
+            {
+                ops |= static_cast<uint64_t>(poll_op::write);
+            }
+
+            if (ops != 0)
+            {
+                poll_ops[i] = static_cast<poll_op>(ops);
+                ++new_sockets;
+            }
+            else
+            {
+                // According to ares usage within curl once a bitmask for a socket is zero the rest of
+                // the bitmask will also be zero.
+                break;
+            }
+        }
+
+        std::vector<coro::task<void>> poll_tasks{};
+        for (size_t i = 0; i < new_sockets; ++i)
+        {
+            auto fd = static_cast<fd_t>(ares_sockets[i]);
+
+            // If this socket is not currently actively polling, start polling!
+            if (m_active_sockets.emplace(fd).second)
+            {
+                m_task_container.start(make_poll_task(fd, poll_ops[i]));
+            }
+        }
+    }
+
+    auto make_poll_task(fd_t fd, poll_op ops) -> coro::task<void>
+    {
+        auto result = co_await m_executor->poll(fd, ops, m_timeout);
+        switch (result)
+        {
+            case poll_status::event:
+            {
+                auto read_sock  = poll_op_readable(ops) ? fd : ARES_SOCKET_BAD;
+                auto write_sock = poll_op_writeable(ops) ? fd : ARES_SOCKET_BAD;
+                ares_process_fd(m_ares_channel, read_sock, write_sock);
+            }
+            break;
+            case poll_status::timeout:
+                ares_process_fd(m_ares_channel, ARES_SOCKET_BAD, ARES_SOCKET_BAD);
+                break;
+            case poll_status::closed:
+                // might need to do something like call with two ARES_SOCKET_BAD?
+                break;
+            case poll_status::error:
+                // might need to do something like call with two ARES_SOCKET_BAD?
+                break;
+        }
+
+        // Remove from the list of actively polling sockets.
+        m_active_sockets.erase(fd);
+
+        // Re-initialize sockets/polls for ares since this one has now triggered.
+        ares_poll();
+
+        co_return;
+    }
+
+    static auto ares_dns_callback(void* arg, int status, int /*timeouts*/, struct hostent* host) -> void
+    {
+        auto& result = *static_cast<coro::net::dns::result<executor_type>*>(arg);
+        --result.m_pending_dns_requests;
+
+        if (host == nullptr || status != ARES_SUCCESS)
+        {
+            result.m_status = status::error;
+        }
+        else
+        {
+            result.m_status = status::complete;
+
+            for (size_t i = 0; host->h_addr_list[i] != nullptr; ++i)
+            {
+                size_t len = (host->h_addrtype == AF_INET) ? net::ip_address::ipv4_len : net::ip_address::ipv6_len;
+                net::ip_address ip_addr{
+                    std::span<const uint8_t>{reinterpret_cast<const uint8_t*>(host->h_addr_list[i]), len},
+                    static_cast<net::domain_t>(host->h_addrtype)};
+
+                result.m_ip_addresses.emplace_back(std::move(ip_addr));
+            }
+        }
+
+        if (result.m_pending_dns_requests == 0)
+        {
+            result.m_resume.set(result.m_executor);
+        }
+    }
 };
 
 } // namespace coro::net::dns
