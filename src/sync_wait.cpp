97b65db38501e5818155d6c878ef9b44c373fa09
jbaldwin <joshrbaldwin@gmail.com>
2024-07-11 14:35:14 -0600

    Use lock for sync_wait completion
    * release/acquire memory ordering has a race condition
    * also reproduced on seq_cst
    
    Closes #270

--- src/sync_wait.cpp
+++ src/sync_wait.cpp
@@ -8,19 +8,22 @@ sync_wait_event::sync_wait_event(bool initially_set) : m_set(initially_set)
 
 auto sync_wait_event::set() noexcept -> void
 {
-    m_set.exchange(true, std::memory_order::release);
+    // issue-270 100~ task's on a thread_pool within sync_wait(when_all) can cause a deadlock/hang if using
+    // release/acquire or even seq_cst.
+    m_set.exchange(true, std::memory_order::seq_cst);
+    std::unique_lock<std::mutex> lk{m_mutex};
     m_cv.notify_all();
 }
 
 auto sync_wait_event::reset() noexcept -> void
 {
-    m_set.exchange(false, std::memory_order::release);
+    m_set.exchange(false, std::memory_order::seq_cst);
 }
 
 auto sync_wait_event::wait() noexcept -> void
 {
     std::unique_lock<std::mutex> lk{m_mutex};
-    m_cv.wait(lk, [this] { return m_set.load(std::memory_order::acquire); });
+    m_cv.wait(lk, [this] { return m_set.load(std::memory_order::seq_cst); });
 }
 
 } // namespace coro::detail
--- test/main.cpp
+++ test/main.cpp
@@ -16,19 +16,20 @@ struct test_setup_networking
         signal(SIGPIPE, SIG_IGN);
 
     #ifdef LIBCORO_FEATURE_TLS
-        // For SSL/TLS tests create a localhost cert.pem and key.pem, tests expected these files
-        // to be generated into the same directory that the tests are running in.
-        auto unused = system(
-            "openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -subj '/CN=localhost' -nodes");
-        (void)unused;
+            // For SSL/TLS tests create a localhost cert.pem and key.pem, tests expected these files
+            // to be generated into the same directory that the tests are running in.
+            // auto unused = system(
+            //     "openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -subj '/CN=localhost'
+            //     -nodes");
+            // (void)unused;
     #endif
     }
 
     ~test_setup_networking()
     {
-        // Cleanup the temporary key.pem and cert.pem files.
-        auto unused = system("rm key.pem cert.pem");
-        (void)unused;
+        // // Cleanup the temporary key.pem and cert.pem files.
+        // auto unused = system("rm key.pem cert.pem");
+        // (void)unused;
     }
 };
 
