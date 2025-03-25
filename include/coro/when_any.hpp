28822dd4996644d964bf687d6e4101a257fe3a7f
jbaldwin <joshrbaldwin@gmail.com>
2025-03-25 06:36:47 -0600

    Add support for coro::task<void> for coro::when_any
    Adds support for return type `void` for `when_any` when using ranges.
    
    Tuple when_any requires the user to specify the return type as
    `std::monostate` instad of `void` because `std::variant` does not allow
    or support `void` as a type per the standard.
    
    Closes #306

--- include/coro/when_any.hpp
+++ include/coro/when_any.hpp
@@ -25,17 +25,13 @@ namespace detail
 
 template<typename return_type, concepts::awaitable awaitable>
 auto make_when_any_tuple_task(
-    coro::mutex&                m,
-    std::atomic<bool>&          return_value_set,
-    coro::event&                notify,
-    std::optional<return_type>& return_value,
-    awaitable                   a) -> coro::task<void>
+    std::atomic<bool>& first_completed, coro::event& notify, std::optional<return_type>& return_value, awaitable a)
+    -> coro::task<void>
 {
-    auto result      = co_await static_cast<awaitable&&>(a);
-    auto scoped_lock = co_await m.lock();
-    if (return_value_set.load(std::memory_order::acquire) == false)
+    auto expected = false;
+    auto result   = co_await static_cast<awaitable&&>(a);
+    if (first_completed.compare_exchange_strong(expected, true, std::memory_order::acq_rel, std::memory_order::relaxed))
     {
-        return_value_set.store(true, std::memory_order::release);
         return_value = std::move(result);
         notify.set();
     }
@@ -48,32 +44,54 @@ template<typename return_type, concepts::awaitable... awaitable_type>
     coro::event& notify, std::optional<return_type>& return_value, awaitable_type... awaitables)
     -> coro::detail::task_self_deleting
 {
-    coro::mutex       m{};
-    std::atomic<bool> return_value_set{false};
+    std::atomic<bool> first_completed{false};
+    co_await when_all(make_when_any_tuple_task(first_completed, notify, return_value, std::move(awaitables))...);
+    co_return;
+}
 
-    co_await when_all(make_when_any_tuple_task(m, return_value_set, notify, return_value, std::move(awaitables))...);
+template<concepts::awaitable awaitable>
+static auto make_when_any_task_return_void(awaitable a, coro::event& notify) -> coro::task<void>
+{
+    co_await static_cast<awaitable&&>(a);
+    notify.set(); // This will trigger the controller task to wake up exactly once.
     co_return;
 }
 
 template<concepts::awaitable awaitable, typename return_type>
 static auto make_when_any_task(
-    awaitable                   a,
-    coro::mutex&                m,
-    std::atomic<bool>&          return_value_set,
-    coro::event&                notify,
-    std::optional<return_type>& return_value) -> coro::task<void>
+    awaitable a, std::atomic<bool>& first_completed, coro::event& notify, std::optional<return_type>& return_value)
+    -> coro::task<void>
 {
-    auto result = co_await static_cast<awaitable&&>(a);
-    co_await m.lock();
+    auto expected = false;
+    auto result   = co_await static_cast<awaitable&&>(a);
     // Its important to only touch return_value and notify once since their lifetimes will be destroyed
-    // after being set ane notified the first time.
-    if (return_value_set.load(std::memory_order::acquire) == false)
+    // after being set and notified the first time.
+    if (first_completed.compare_exchange_strong(expected, true, std::memory_order::acq_rel, std::memory_order::relaxed))
     {
-        return_value_set.store(true, std::memory_order::release);
         return_value = std::move(result);
         notify.set();
     }
 
+    co_return;
+}
+
+template<std::ranges::range range_type, concepts::awaitable awaitable_type = std::ranges::range_value_t<range_type>>
+static auto make_when_any_controller_task_return_void(range_type awaitables, coro::event& notify)
+    -> coro::detail::task_self_deleting
+{
+    std::vector<coro::task<void>> tasks{};
+
+    if constexpr (std::ranges::sized_range<range_type>)
+    {
+        tasks.reserve(std::size(awaitables));
+    }
+
+    for (auto&& a : awaitables)
+    {
+        tasks.emplace_back(make_when_any_task_return_void<awaitable_type>(std::move(a), notify));
+    }
+
+    co_await coro::when_all(std::move(tasks));
     co_return;
 }
 
@@ -86,11 +104,10 @@ static auto make_when_any_controller_task(
     range_type awaitables, coro::event& notify, std::optional<return_type_base>& return_value)
     -> coro::detail::task_self_deleting
 {
-    // These must live for as long as the longest running when_any task since each task tries to see
+    // This must live for as long as the longest running when_any task since each task tries to see
     // if it was the first to complete. Only the very first task to complete will set the return_value
     // and notify.
-    coro::mutex       m{};
-    std::atomic<bool> return_value_set{false};
+    std::atomic<bool> first_completed{false};
 
     // This detatched task will maintain the lifetime of all the when_any tasks.
     std::vector<coro::task<void>> tasks{};
@@ -102,8 +119,8 @@ static auto make_when_any_controller_task(
 
     for (auto&& a : awaitables)
     {
-        tasks.emplace_back(make_when_any_task<awaitable_type, return_type_base>(
-            std::move(a), m, return_value_set, notify, return_value));
+        tasks.emplace_back(
+            make_when_any_task<awaitable_type, return_type_base>(std::move(a), first_completed, notify, return_value));
     }
 
     co_await coro::when_all(std::move(tasks));
@@ -119,10 +136,9 @@ template<concepts::awaitable... awaitable_type>
     using return_type = std::variant<
         std::remove_reference_t<typename concepts::awaitable_traits<awaitable_type>::awaiter_return_type>...>;
 
-    std::optional<return_type> return_value{std::nullopt};
     coro::event                notify{};
-
-    auto controller_task =
+    std::optional<return_type> return_value{std::nullopt};
+    auto                       controller_task =
         detail::make_when_any_tuple_controller_task(notify, return_value, std::forward<awaitable_type>(awaitables)...);
     controller_task.handle().resume();
 
@@ -138,10 +154,9 @@ template<concepts::awaitable... awaitable_type>
     using return_type = std::variant<
         std::remove_reference_t<typename concepts::awaitable_traits<awaitable_type>::awaiter_return_type>...>;
 
-    std::optional<return_type> return_value{std::nullopt};
     coro::event                notify{};
-
-    auto controller_task =
+    std::optional<return_type> return_value{std::nullopt};
+    auto                       controller_task =
         detail::make_when_any_tuple_controller_task(notify, return_value, std::forward<awaitable_type>(awaitables)...);
     controller_task.handle().resume();
 
@@ -156,17 +171,32 @@ template<
     typename return_type_base          = std::remove_reference_t<return_type>>
 [[nodiscard]] auto when_any(std::stop_source stop_source, range_type awaitables) -> coro::task<return_type_base>
 {
-    // Using an std::optional to prevent the need to default construct the type on the stack.
-    std::optional<return_type_base> return_value{std::nullopt};
-    coro::event                     notify{};
+    coro::event notify{};
 
-    auto controller_task =
-        detail::make_when_any_controller_task(std::forward<range_type>(awaitables), notify, return_value);
-    controller_task.handle().resume();
+    if constexpr (std::is_void_v<return_type_base>)
+    {
+        auto controller_task =
+            detail::make_when_any_controller_task_return_void(std::forward<range_type>(awaitables), notify);
+        controller_task.handle().resume();
 
-    co_await notify;
-    stop_source.request_stop();
-    co_return std::move(return_value.value());
+        co_await notify;
+        stop_source.request_stop();
+        co_return;
+    }
+    else
+    {
+        // Using an std::optional to prevent the need to default construct the type on the stack.
+        std::optional<return_type_base> return_value{std::nullopt};
+
+        auto controller_task =
+            detail::make_when_any_controller_task(std::forward<range_type>(awaitables), notify, return_value);
+        controller_task.handle().resume();
+
+        co_await notify;
+        stop_source.request_stop();
+
+        co_return std::move(return_value.value());
+    }
 }
 
 template<
@@ -176,15 +206,29 @@ template<
     typename return_type_base          = std::remove_reference_t<return_type>>
 [[nodiscard]] auto when_any(range_type awaitables) -> coro::task<return_type_base>
 {
-    std::optional<return_type_base> return_value{std::nullopt};
-    coro::event                     notify{};
+    coro::event notify{};
 
-    auto controller_task =
-        detail::make_when_any_controller_task(std::forward<range_type>(awaitables), notify, return_value);
-    controller_task.handle().resume();
+    if constexpr (std::is_void_v<return_type_base>)
+    {
+        auto controller_task =
+            detail::make_when_any_controller_task_return_void(std::forward<range_type>(awaitables), notify);
+        controller_task.handle().resume();
 
-    co_await notify;
-    co_return std::move(return_value.value());
+        co_await notify;
+        co_return;
+    }
+    else
+    {
+        std::optional<return_type_base> return_value{std::nullopt};
+
+        auto controller_task =
+            detail::make_when_any_controller_task(std::forward<range_type>(awaitables), notify, return_value);
+        controller_task.handle().resume();
+
+        co_await notify;
+
+        co_return std::move(return_value.value());
+    }
 }
 
 } // namespace coro
