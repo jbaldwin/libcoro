e0efc064bd816ed239794886094c33337740bad7
Ripose <ripose@protonmail.com>
2025-06-20 22:32:00 -0700

    task_container: add back task_container
    Adds back coro::task_container. Since this has been removed, there has
    been no easy way to share a thread_pool/io_scheduler across an
    application, while retaining the ability to await the completion of a
    subset of tasks.
    
    Task containers are also useful in cases where a class starts a task on
    a thread pool that references itself. If the task lives longer than the
    class, that reference begins to dangle. Task containers solve this by
    allowing the class to yield_until_empty() in the destructor to avoid
    dangling references.
    
    Task containers can also function as latches in cases where
    coro::when_all is insufficient. If the data required to start multiple
    tasks becomes available at different times, tasks can be started
    immediately on the task container and then yield_until_empty() can
    function as the latch when necessary.

---
+++ test/test_task_container.cpp
@@ -0,0 +1,61 @@
+#include "catch_amalgamated.hpp"
+
+#include <coro/coro.hpp>
+
+#include <atomic>
+#include <iostream>
+
+TEST_CASE("task_container", "[task_container]")
+{
+    std::cerr << "[task_container]\n\n";
+}
+
+using namespace std::chrono_literals;
+
+TEST_CASE("task_container schedule single task", "[task_container]")
+{
+    auto s = coro::thread_pool::make_shared(
+        coro::thread_pool::options{.thread_count = 1});
+
+    int value = 0;
+    auto make_task = [] (int &value) -> coro::task<void>
+    {
+        value = 37;
+        co_return;
+    };
+
+    coro::task_container<coro::thread_pool> tc{s};
+    tc.start(make_task(value));
+    REQUIRE(tc.size() == 1);
+    coro::sync_wait(tc.yield_until_empty());
+    REQUIRE(value == 37);
+    REQUIRE(tc.empty());
+}
+
+TEST_CASE("task_container submit mutiple tasks", "[task_container]")
+{
+    constexpr std::size_t n = 1000;
+    std::atomic<uint64_t> counter{0};
+    auto s = coro::thread_pool::make_shared(
+        coro::thread_pool::options{.thread_count = 1});
+    coro::task_container<coro::thread_pool> tc{s};
+
+    auto make_task = [](std::atomic<uint64_t>& counter) -> coro::task<void>
+    {
+        counter++;
+        co_return;
+    };
+    for (std::size_t i = 0; i < n; ++i)
+    {
+        tc.start(make_task(counter));
+    }
+
+    coro::sync_wait(tc.yield_until_empty());
+    REQUIRE(counter == n);
+    REQUIRE(tc.empty());
+}
+
+TEST_CASE("~task_container", "[task_container]")
+{
+    std::cerr << "[~task_container]\n\n";
+}
