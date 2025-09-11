cf5af3613499c326428db32e71d5bf89e065c14b
jbaldwin <joshrbaldwin@gmail.com>
2025-09-11 13:23:02 -0600

    const|mutable buffer concepts less restrictive.
    Previously only allowed for [const] char* buffers, this should allow for
    anything that is trivially copyable and buffer like to work.
    
    Closes #390

--- include/coro/concepts/buffer.hpp
+++ include/coro/concepts/buffer.hpp
@@ -10,17 +10,31 @@ namespace coro::concepts
 template<typename type>
 concept const_buffer = requires(const type t)
 {
+    // The underlying data type must be a standard-layout type and trivially copyable
+    typename std::remove_pointer_t<decltype(t.data())>;
+    requires std::is_trivial_v<std::remove_pointer_t<decltype(t.data())>>;
+
+    // General buffer properties
     { t.empty() } -> std::same_as<bool>;
-    { t.data() } -> std::same_as<const char*>;
     { t.size() } -> std::same_as<std::size_t>;
+
+    // We check the return type of `data()` to be a const pointer to the underlying type
+    { t.data() } -> std::same_as<const typename std::remove_pointer_t<decltype(t.data())>*>;
 };
 
 template<typename type>
 concept mutable_buffer = requires(type t)
 {
+    // The underlying data type must be a standard-layout type and trivially copyable
+    typename std::remove_pointer_t<decltype(t.data())>;
+    requires std::is_trivial_v<std::remove_pointer_t<decltype(t.data())>>;
+
+    // General buffer properties
     { t.empty() } -> std::same_as<bool>;
-    { t.data() } -> std::same_as<char*>;
     { t.size() } -> std::same_as<std::size_t>;
+
+    // We check the return type of `data()` to be a non-const pointer to the underlying type
+    { t.data() } -> std::same_as<typename std::remove_pointer_t<decltype(t.data())>*>;
 };
 // clang-format on
 
