c301d7df8a6ad13bfb51a7037bd72505b3d606ec
Josh Baldwin <jbaldwin@users.noreply.github.com>
2025-09-11 14:45:55 -0600

    const|mutable buffer concepts less restrictive. (#391)
    Previously only allowed for [const] char* buffers, this should allow for
    anything that is trivially copyable and buffer like to work.
    
    Closes #390

---
+++ test/concepts/test_concepts.cpp
@@ -0,0 +1,59 @@
+#include "catch_amalgamated.hpp"
+
+#include <coro/coro.hpp>
+
+TEST_CASE("concepts", "[concepts]")
+{
+    std::cerr << "[concepts]\n\n";
+}
+
+struct char_buffer {
+    bool empty() const { return false; }
+    const char* data() const { return nullptr; }
+    std::size_t size() const { return 0; }
+    char* data() { return nullptr; }
+};
+
+struct uint8_buffer {
+    bool empty() const { return false; }
+    const uint8_t* data() const { return nullptr; }
+    std::size_t size() const { return 0; }
+    uint8_t* data() { return nullptr; }
+};
+
+struct std_byte_buffer {
+    bool empty() const { return false; }
+    const std::byte* data() const { return nullptr; }
+    std::size_t size() const { return 0; }
+    std::byte* data() { return nullptr; }
+};
+
+using coro::concepts::mutable_buffer;
+using coro::concepts::const_buffer;
+
+TEST_CASE("mutable_buffer", "[concepts]")
+{
+    static_assert(mutable_buffer<char_buffer>);
+    static_assert(mutable_buffer<uint8_buffer>);
+    static_assert(mutable_buffer<std_byte_buffer>);
+    static_assert(mutable_buffer<std::vector<char>>);
+    static_assert(mutable_buffer<std::vector<uint8_t>>);
+
+    REQUIRE(true);
+}
+
+TEST_CASE("const_buffer", "[concepts]")
+{
+    static_assert(const_buffer<char_buffer>);
+    static_assert(const_buffer<uint8_buffer>);
+    static_assert(const_buffer<std_byte_buffer>);
+    static_assert(const_buffer<std::vector<char>>);
+    static_assert(const_buffer<std::vector<uint8_t>>);
+
+    REQUIRE(true);
+}
+
+TEST_CASE("~concepts", "[concepts]")
+{
+    std::cerr << "[~concepts]\n\n";
+}
