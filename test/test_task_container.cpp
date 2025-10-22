131d9ff6f45b09cd092aaa282846424205eb1915
Lawrence Mitchell <lmitchell@nvidia.com>
2025-10-22 14:00:46 +0100

    Store raw pointer in udp::peer and implement copy/move-assignment (#403)
    * Store raw pointer in udp::peer and implement copy/move-assignment
    
    Previously the peer struct held a reference member that could not be
    reseated and so the defaulted copy/move-assignments were implicitly
    deleted. We want these objects to be moveable and copyable so instead store
    a raw pointer.
    
    * Remove potential for false-positive fail in task_container test
    
    Since we detach our (fast to run) task, it might complete on the
    thread_pool thread before we get time-sliced in to check its size on the
    main thread, leading to a false positive failure in that the container is
    already empty.
    
    To fix this, use an event to ensure the task does not complete until we've
    checked the container's size.

--- test/test_task_container.cpp
+++ test/test_task_container.cpp
@@ -14,19 +14,22 @@ using namespace std::chrono_literals;
 
 TEST_CASE("task_container schedule single task", "[task_container]")
 {
-    auto s = coro::thread_pool::make_unique(
-        coro::thread_pool::options{.thread_count = 1});
-
-    int value = 0;
-    auto make_task = [] (int &value) -> coro::task<void>
+    auto                    s = coro::thread_pool::make_unique(coro::thread_pool::options{.thread_count = 1});
+    coro::event             can_start{false};
+    int                     value = 0;
+    auto make_task = [](int& value, coro::event& can_start) -> coro::task<void>
     {
+        co_await can_start;
         value = 37;
         co_return;
     };
 
     coro::task_container<coro::thread_pool> tc{s};
-    tc.start(make_task(value));
+    tc.start(make_task(value, can_start));
     REQUIRE(tc.size() == 1);
+    // Only allow the task to complete once we have checked the container's size,
+    // otherwise the require could "spuriously" fail due to the task already being done.
+    can_start.set(s);
     coro::sync_wait(tc.yield_until_empty());
     REQUIRE(value == 37);
     REQUIRE(tc.empty());
