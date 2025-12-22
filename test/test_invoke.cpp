8c6dc5997d6ef8596d127c22a1852a15a0c33faf
jbaldwin <joshrbaldwin@gmail.com>
2025-12-22 10:11:34 -0700

    Make tests destroy captures and arguments after first suspend

--- test/test_invoke.cpp
+++ test/test_invoke.cpp
@@ -10,39 +10,48 @@ TEST_CASE("invoke", "[invoke]")
 
 TEST_CASE("invoke no captures", "[invoke]")
 {
-    int a = 1;
-    int b = 2;
-    auto make_task = [](int a, int b) -> coro::task<int> { co_return a + b; };
-    auto task = coro::invoke(make_task, a, b);
+    coro::task<int> task;
+    coro::event suspended{}; // capture by ref since its on stable stack frame location for this test
 
+    {
+        int a = 1;
+        int b = 2;
+        auto make_task = [&suspended](int a, int b) -> coro::task<int>
+        {
+            co_await suspended;
+            co_return a + b;
+        };
+        task = coro::invoke(make_task, a, b);
+        task.resume(); // Start the task up to the first suspension point.
+        // Let the argument variables go out of scope to guarantee they are destroyed.
+    }
+
+    // Resume the task, it should have copied all variables to its own coroutine frame.
+    suspended.set();
     REQUIRE(coro::sync_wait(task) == 3);
 }
 
 TEST_CASE("invoke captures", "[invoke]")
 {
-    int a = 1;
-    int b = 2;
-    int c = 3;
-    auto make_task = [&c](int a, int b) -> coro::task<int> { co_return a + b + c; };
-    auto task = coro::invoke(make_task, a, b);
+    coro::task<int> task;
+    coro::event suspended{};
 
-    REQUIRE(coro::sync_wait(task) == 6);
-}
+    {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        auto make_task = [&suspended, c, d](int a, int b) -> coro::task<int>
+        {
+            co_await suspended;
+            co_return a + b + c + d;
+        };
+        task = coro::invoke(make_task, a, b);
+        task.resume();
+    }
 
-TEST_CASE("invoke captures with suspend", "[invoke]")
-{
-    auto tp = coro::thread_pool::make_unique(coro::thread_pool::options{.thread_count = 1});
-
-    int a = 1;
-    int b = 2;
-    int c = 3;
-    auto make_task = [&tp, &c](int a, int b) -> coro::task<int> {
-        co_await tp->schedule();
-        co_return a + b + c;
-    };
-    auto task = coro::invoke(make_task, a, b);
-
-    REQUIRE(coro::sync_wait(task) == 6);
+    suspended.set();
+    REQUIRE(coro::sync_wait(task) == 10);
 }
 
 TEST_CASE("~invoke", "[invoke]")
