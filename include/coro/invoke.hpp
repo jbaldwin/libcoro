0788332a5052b1d223824c939dfe41c630c0f1f5
jbaldwin <joshrbaldwin@gmail.com>
2025-12-22 13:40:08 -0700

    Use invoker task to auto-start
    * invoke() is now a regular function
    * returns a started coro::task<T> that the user can await for completion

--- include/coro/invoke.hpp
+++ include/coro/invoke.hpp
@@ -5,6 +5,18 @@
 namespace coro
 {
 
+namespace detail
+{
+
+template<typename functor_type, typename... args_types>
+auto make_invoker_task(functor_type functor, args_types&&... args) -> decltype(functor(std::forward<args_types>(args)...))
+{
+    auto user_task = functor(std::forward<args_types>(args)...);
+    co_return co_await user_task;
+}
+
+} // namespace detail
+
 /**
  * @brief Invokes the given functor as a coroutine. This is useful if you want
  *        to use lambda captures and need them to be captured onto a stable
@@ -35,9 +47,13 @@ namespace coro
  * @return functor_type -> return_type
  */
 template<typename functor_type, typename... args_types>
-auto invoke(functor_type functor, args_types&&... args) -> decltype(functor(std::forward<args_types>(args)...))
+auto invoke(functor_type functor, args_types&&... args) -> decltype(auto)
 {
-    co_return co_await functor(std::forward<args_types>(args)...);
+    auto invoker_task = detail::make_invoker_task(std::forward<functor_type>(functor), std::forward<args_types>(args)...);
+    invoker_task.resume();
+    return invoker_task;
+
+    //co_return co_await functor(std::forward<args_types>(args)...);
 }
 
 } // namespace coro
--- test/test_invoke.cpp
+++ test/test_invoke.cpp
@@ -11,47 +11,86 @@ TEST_CASE("invoke", "[invoke]")
 TEST_CASE("invoke no captures", "[invoke]")
 {
     coro::task<int> task;
-    coro::event suspended{}; // capture by ref since its on stable stack frame location for this test
 
     {
         int a = 1;
         int b = 2;
-        auto make_task = [&suspended](int a, int b) -> coro::task<int>
-        {
-            co_await suspended;
-            co_return a + b;
-        };
+        auto make_task = [](int a, int b) -> coro::task<int> { co_return a + b; };
         task = coro::invoke(make_task, a, b);
-        task.resume(); // Start the task up to the first suspension point.
-        // Let the argument variables go out of scope to guarantee they are destroyed.
+        // Let the argument variables go out of scope to guarantee they are destroyed on this local scope.
     }
 
-    // Resume the task, it should have copied all variables to its own coroutine frame.
-    suspended.set();
     REQUIRE(coro::sync_wait(task) == 3);
 }
 
 TEST_CASE("invoke captures", "[invoke]")
 {
     coro::task<int> task;
-    coro::event suspended{};
 
     {
         int a = 1;
         int b = 2;
         int c = 3;
         int d = 4;
-        auto make_task = [&suspended, c, d](int a, int b) -> coro::task<int>
+        auto make_task = [c, d](int a, int b) -> coro::task<int>
         {
-            co_await suspended;
             co_return a + b + c + d;
         };
         task = coro::invoke(make_task, a, b);
-        task.resume();
     }
 
-    suspended.set();
     REQUIRE(coro::sync_wait(task) == 10);
+}
+
+TEST_CASE("invoke captures in coroutine context", "[invoke]")
+{
+    auto tp = coro::thread_pool::make_unique(coro::thread_pool::options{.thread_count = 1});
+    coro::task<int> task;
+
+    auto make_root_task = [](std::unique_ptr<coro::thread_pool>& tp) -> coro::task<int>
+    {
+        co_await tp->schedule();
+
+        coro::task<int> user_task;
+        {
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            int d = 4;
+            auto make_user_task = [c, d](int a, int b) -> coro::task<int>
+            {
+                co_return a + b + c + d;
+            };
+            user_task = coro::invoke(make_user_task, a, b);
+        }
+
+        co_return co_await user_task;
+    };
+
+    REQUIRE(coro::sync_wait(make_root_task(tp)) == 10);
+}
+
+TEST_CASE("invoke co_await coro::invoke", "[invoke]")
+{
+    auto tp = coro::thread_pool::make_unique(coro::thread_pool::options{.thread_count = 1});
+    coro::task<int> task;
+
+    auto make_root_task = [](std::unique_ptr<coro::thread_pool>& tp) -> coro::task<int>
+    {
+        co_await tp->schedule();
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        auto make_user_task = [c, d](int a, int b) -> coro::task<int>
+        {
+            co_return a + b + c + d;
+        };
+        co_return co_await coro::invoke(make_user_task, a, b);
+    };
+
+    REQUIRE(coro::sync_wait(make_root_task(tp)) == 10);
 }
 
 TEST_CASE("~invoke", "[invoke]")
