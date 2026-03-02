1fed540a291c9f5d8c89bd58480ed9f6d2409396
PyXiion <me@pyxiion.ru>
2026-03-02 16:41:49 +0300

    Allow socket_stream to own client

--- include/coro/ranges/socket_stream.hpp
+++ include/coro/ranges/socket_stream.hpp
@@ -5,12 +5,12 @@
 
 namespace coro::ranges
 {
-template<class buffer_t>
+template<class client_t, class buffer_t>
 class socket_stream
 {
 public:
-    explicit socket_stream(coro::net::tcp::client& client, buffer_t&& buffer)
-        : m_client(client),
+    explicit socket_stream(client_t&& client, buffer_t&& buffer)
+        : m_client(std::forward<client_t>(client)),
           m_buffer{std::move(buffer)} {};
 
     auto advance() -> coro::task<bool>
@@ -35,20 +35,33 @@ public:
     auto get_value() const noexcept -> std::span<const std::byte> { return {m_buffer.data(), m_current_size}; }
 
 private:
-    coro::net::tcp::client& m_client;
-    buffer_t                m_buffer;
-    std::size_t             m_current_size{};
+    client_t    m_client;
+    buffer_t    m_buffer;
+    std::size_t m_current_size{};
 };
 
 auto to_chunked_stream(coro::net::tcp::client& client, std::size_t buffer_size = 4096)
-    -> socket_stream<std::vector<std::byte>>
+    -> socket_stream<coro::net::tcp::client&, std::vector<std::byte>>
 {
-    return socket_stream{client, std::vector<std::byte>{buffer_size}};
+    return socket_stream<coro::net::tcp::client&, std::vector<std::byte>>{client, std::vector<std::byte>{buffer_size}};
 }
 
 auto to_chunked_stream(coro::net::tcp::client& client, std::span<std::byte> external_buffer)
-    -> socket_stream<std::span<std::byte>>
+    -> socket_stream<coro::net::tcp::client&, std::span<std::byte>>
 {
-    return socket_stream{client, std::move(external_buffer)};
+    return socket_stream<coro::net::tcp::client&, std::span<std::byte>>{client, std::move(external_buffer)};
+}
+
+auto to_chunked_stream(coro::net::tcp::client&& client, std::size_t buffer_size = 4096)
+    -> socket_stream<coro::net::tcp::client, std::vector<std::byte>>
+{
+    return socket_stream<coro::net::tcp::client, std::vector<std::byte>>{
+        std::move(client), std::vector<std::byte>{buffer_size}};
+}
+
+auto to_chunked_stream(coro::net::tcp::client&& client, std::span<std::byte> external_buffer)
+    -> socket_stream<coro::net::tcp::client, std::span<std::byte>>
+{
+    return socket_stream<coro::net::tcp::client, std::span<std::byte>>{std::move(client), std::move(external_buffer)};
 }
 } // namespace coro::ranges
--- test/test_ranges.cpp
+++ test/test_ranges.cpp
@@ -1,5 +1,6 @@
 #include "coro/ranges/join.hpp"
 #include "coro/ranges/transform.hpp"
+#include "net/catch_net_extensions.hpp"
 #include <catch_amalgamated.hpp>
 #include <coro/coro.hpp>
 #include <coro/ranges/socket_stream.hpp>
@@ -7,47 +8,58 @@
 #include <coro/ranges/to.hpp>
 #include <iostream>
 
-TEST_CASE("", "[async_ranges]")
+auto make_server_task(
+    std::unique_ptr<coro::scheduler>& scheduler, const coro::net::socket_address& address, std::string_view message)
+    -> coro::task<void>
 {
+    co_await scheduler->schedule();
+
+    auto server = coro::net::tcp::server(scheduler, address);
+
+    auto client = co_await server.accept();
+    REQUIRE(client);
+    std::cerr << "server: accepted\n";
+
+    auto [wstatus, written] = co_await client->write_all(message);
+    REQUIRE_THAT(wstatus, IsOk());
+    std::cerr << "server: sent message\n";
+}
+
+auto make_client_stream(std::unique_ptr<coro::scheduler>& scheduler, const coro::net::socket_address& address)
+    -> coro::task<coro::ranges::socket_stream<coro::net::tcp::client, std::vector<std::byte>>>
+{
+    co_await scheduler->schedule();
+    auto client = coro::net::tcp::client{scheduler, address};
+
+    auto cstatus = co_await client.connect();
+    REQUIRE(cstatus == coro::net::connect_status::connected);
+    std::cerr << "client: connected\n";
+
+    co_return coro::ranges::to_chunked_stream(std::move(client));
+}
+
+TEST_CASE("Stream to string", "[async_ranges]")
+{
+    static auto address = coro::net::socket_address{"127.0.0.1", 8080};
+
+    auto [message, expected] = GENERATE(table<std::string_view, std::string_view>({{"Hello world!", "Hello world!"}}));
+
     auto scheduler = coro::scheduler::make_unique();
 
-    auto server_task = [](std::unique_ptr<coro::scheduler>& scheduler) -> coro::task<void>
+    auto server = make_server_task(scheduler, address, message);
+
+    auto client = [](std::unique_ptr<coro::scheduler>& scheduler, std::string_view message) -> coro::task<void>
     {
         co_await scheduler->schedule();
+        auto stream = co_await make_client_stream(scheduler, address);
 
-        auto server = coro::net::tcp::server{scheduler, {"127.0.0.1", 8080}};
+        auto pipeline = std::move(stream) | coro::ranges::join |
+                        coro::ranges::transform([](auto byte) { return static_cast<char>(byte); }) |
+                        coro::ranges::to<std::string>;
 
-        auto client = co_await server.accept();
-        std::cerr << "server: accepted client\n";
+        auto result = co_await pipeline;
+        CHECK(result == message);
+    }(scheduler, expected);
 
-        if (not client)
-        {
-            throw std::runtime_error(client.error().message());
-        }
-
-        std::string data{"Hello!\0 Hidden"};
-        co_await client->write_all(data);
-        std::cerr << "server: sent message\n";
-    };
-    auto client_task = [](std::unique_ptr<coro::scheduler>& scheduler) -> coro::task<void>
-    {
-        co_await scheduler->schedule();
-
-        auto client = coro::net::tcp::client{scheduler, {"127.0.0.1", 8080}};
-
-        co_await client.connect();
-        std::cerr << "client: connected\n";
-
-        auto result = co_await (
-            coro::ranges::to_chunked_stream(client) | coro::ranges::join |
-            coro::ranges::transform([](auto f) { return static_cast<char>(f); }) |
-            coro::ranges::take_until([](auto f) { return static_cast<char>(f) == '0'; }) |
-            coro::ranges::to<std::string>);
-
-        std::cerr << "client: received data\n";
-
-        std::cerr << result << ' ';
-    };
-
-    coro::sync_wait(coro::when_all(server_task(scheduler), client_task(scheduler)));
+    coro::sync_wait(coro::when_all(std::move(server), std::move(client)));
 }
