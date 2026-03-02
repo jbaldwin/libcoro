05b1ff2ec54cdb9c20124e9937d33d3b886e7849
PyXiion <me@pyxiion.ru>
2026-03-02 15:15:51 +0300

    Add `to<std::string>`

--- include/coro/ranges/to.hpp
+++ include/coro/ranges/to.hpp
@@ -22,6 +22,26 @@ public:
 private:
 };
 
+template<class char_t, class traits, class alloc_t>
+struct _to<std::basic_string<char_t, traits, alloc_t>> : public concepts::_async_adaptor
+{
+    using container_t = std::basic_string<char_t, traits, alloc_t>;
+
+public:
+    template<concepts::async_streamable Rng>
+    auto operator()(Rng rng) const -> coro::task<container_t>
+    {
+        container_t result;
+        while (co_await rng.advance())
+        {
+            result.push_back(std::move(rng.get_value()));
+        }
+        co_return result;
+    }
+
+private:
+};
+
 template<class container_t>
 inline constexpr _to<container_t> to;
 } // namespace coro::ranges
